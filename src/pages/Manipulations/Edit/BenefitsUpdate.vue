ed4cfd3723fe4f31b72e7c6a4c146428872e47a6
Marvin Nchena <linosnchena@gmail.com>
2024-06-08 18:01:07 +0200

    lusakax

--- src/pages/Manipulations/Edit/BenefitsUpdate.vue
+++ src/pages/Manipulations/Edit/BenefitsUpdate.vue
@@ -4,92 +4,173 @@
     <h3 class="head">{{ title.toUpperCase() }}</h3>
 
     <div v-if="payCheck">
-      <v-card class="mx-auto justify-space-evenly customa-portfolio" color="#D7CCC8" theme="light" width="100vw"
-        height="95vh" prepend-icon="mdi-rhombus-outline" title="Employee Paycheck and Medical Benefits | Updating">
-        <v-form ref="form" class="mx-2" v-model="validfx" lazy-validation @submit.prevent="updateEmployeesBenefits">
+      <v-card
+        class="mx-auto justify-space-evenly customa-portfolio"
+        color="#D7CCC8"
+        theme="light"
+        width="100vw"
+        height="95vh"
+        prepend-icon="mdi-rhombus-outline"
+        title="Employee Paycheck and Medical Benefits | Updating"
+      >
+        <v-form
+          ref="form"
+          class="mx-2"
+          v-model="validfx"
+          lazy-validation
+          @submit.prevent="updateEmployeesBenefits"
+        >
           <v-alert v-if="!validfx" type="error">
             Period is out of range. Please correct the invalid input data.
           </v-alert>
 
           <v-row class="mx-auto justify-space-evenly">
             <v-col cols="5">
-              <h4>EMPLOYEE RECORDS</h4><br>
-              <v-text-field v-model="payCheck.namex" label="01 Customer name" bg-color="teal"
-                variant="solo"></v-text-field>
-              <v-text-field v-model="payCheck.emailx" label="02 Email" variant="outlined"></v-text-field>
-              <v-select v-model="payCheck.marriage" label="03 Marriage" variant="outlined" :items="marriage"></v-select>
-              <v-text-field v-model="payCheck.grosspay" label="04 Gross Pay" prefix="K"
-                variant="outlined"></v-text-field>
-              <v-text-field type="text" label="05 Period Month" v-model="payCheck.periodMonth"
-                variant="outlined"></v-text-field>
-              <v-select v-model="payCheck.periodYear" label="06 Period year" variant="outlined"
-                :items="years"></v-select>
+              <h4>EMPLOYEE RECORDS</h4>
+              <br />
+              <v-text-field
+                v-model="payCheck.namex"
+                label="01 Customer name"
+                bg-color="teal"
+                variant="solo"
+              ></v-text-field>
+              <v-text-field
+                v-model="payCheck.emailx"
+                label="02 Email"
+                variant="outlined"
+              ></v-text-field>
+              <v-select
+                v-model="payCheck.marriage"
+                label="03 Marriage"
+                variant="outlined"
+                :items="marriage"
+              ></v-select>
+              <v-text-field
+                v-model="payCheck.grosspay"
+                label="04 Gross Pay"
+                prefix="K"
+                variant="outlined"
+              ></v-text-field>
+              <v-text-field
+                type="text"
+                label="05 Period Month"
+                v-model="payCheck.periodMonth"
+                variant="outlined"
+              ></v-text-field>
+              <v-select
+                v-model="payCheck.periodYear"
+                label="06 Period year"
+                variant="outlined"
+                :items="years"
+              ></v-select>
             </v-col>
 
             <v-col cols="5" v-if="payCheck && payCheck.families">
               <h4>
                 FAMILY SCREEN | {{ payCheck.families.length }} |
                 {{ payCheck.children }}
-              </h4><br>
+              </h4>
+              <br />
               <v-row>
                 <v-col cols="6">
-                  <v-select v-model="payCheck.children" label="01 Children" variant="outlined"
-                    :items="childrenx"></v-select>
+                  <v-select
+                    v-model="payCheck.children"
+                    label="01 Children"
+                    variant="outlined"
+                    :items="childrenx"
+                  ></v-select>
                 </v-col>
 
                 <v-col cols="6">
-                  <v-text-field v-model="payCheck.families.length" label="02 Family Size"
-                    variant="outlined"></v-text-field>
+                  <v-text-field
+                    v-model="payCheck.families.length"
+                    label="02 Family Size"
+                    variant="outlined"
+                  ></v-text-field>
                 </v-col>
               </v-row>
 
               <v-row v-if="payCheck.families[0]">
                 <v-col cols="6" v-if="payCheck.children > 0">
-                  <v-text-field v-model="payCheck.families[0].namex" label="01 Name oF Child One"
-                    variant="outlined"></v-text-field>
+                  <v-text-field
+                    v-model="payCheck.families[0].namex"
+                    label="01 Name oF Child One"
+                    variant="outlined"
+                  ></v-text-field>
                 </v-col>
                 <v-col cols="6" v-if="payCheck.children > 0">
-                  <v-select v-model="payCheck.families[0].gender" label="01  Gender of Child" variant="outlined"
-                    :items="genders"></v-select>
+                  <v-select
+                    v-model="payCheck.families[0].gender"
+                    label="01  Gender of Child"
+                    variant="outlined"
+                    :items="genders"
+                  ></v-select>
                 </v-col>
               </v-row>
 
               <v-row v-if="payCheck.families[1]">
                 <v-col cols="6" v-if="payCheck.children > 1">
-                  <v-text-field v-model="payCheck.families[1].namex" label="01 Name oF Child One"
-                    variant="outlined"></v-text-field>
+                  <v-text-field
+                    v-model="payCheck.families[1].namex"
+                    label="01 Name oF Child One"
+                    variant="outlined"
+                  ></v-text-field>
                 </v-col>
                 <v-col cols="6" v-if="payCheck.children > 1">
-                  <v-select v-model="payCheck.families[1].gender" label="01  Gender of Child" variant="outlined"
-                    :items="genders"></v-select>
+                  <v-select
+                    v-model="payCheck.families[1].gender"
+                    label="01  Gender of Child"
+                    variant="outlined"
+                    :items="genders"
+                  ></v-select>
                 </v-col>
               </v-row>
 
               <v-row v-if="payCheck.families[2]">
                 <v-col cols="6" v-if="payCheck.children > 2">
-                  <v-text-field v-model="payCheck.families[2].namex" label="01 Name oF Child One"
-                    variant="outlined"></v-text-field>
+                  <v-text-field
+                    v-model="payCheck.families[2].namex"
+                    label="01 Name oF Child One"
+                    variant="outlined"
+                  ></v-text-field>
                 </v-col>
                 <v-col cols="6" v-if="payCheck.children > 2">
-                  <v-select v-model="payCheck.families[2].gender" label="01  Gender of Child" variant="outlined"
-                    :items="genders"></v-select>
+                  <v-select
+                    v-model="payCheck.families[2].gender"
+                    label="01  Gender of Child"
+                    variant="outlined"
+                    :items="genders"
+                  ></v-select>
                 </v-col>
               </v-row>
 
               <div v-if="payCheck.children">
                 Approved Children : | {{ payCheck.families.length }} /
-                {{ payCheck.children }} | | Balance | {{ balancex }} / {{ payCheck.children }}
-                <br />
-                New Child
-                <br />
+                {{ payCheck.children }} | | Balance | {{ balancex }} /
+                {{ payCheck.children }}
                 <br />
                 <v-row v-if="balancex">
+                  <br />
+                  <br />
+                  New Child
+                  <br />
+                </v-row>
+                <v-row v-if="balancex">
+                  <br />
                   <v-col cols="6" v-if="balancex > 0">
-                    <v-text-field v-model="name1" label="01 Name oF Child One" variant="outlined"></v-text-field>
+                    <v-text-field
+                      v-model="name1"
+                      label="01 Name oF Child One"
+                      variant="outlined"
+                    ></v-text-field>
                   </v-col>
                   <v-col cols="6" v-if="balancex > 0">
-                    <v-select v-model="gender1" label="01  Gender of Child" variant="outlined"
-                      :items="genders"></v-select>
+                    <v-select
+                      v-model="gender1"
+                      label="01  Gender of Child"
+                      variant="outlined"
+                      :items="genders"
+                    ></v-select>
                   </v-col>
                 </v-row>
               </div>
@@ -97,14 +178,31 @@
           </v-row>
 
           <v-card-actions class="mx-auto justify-space-evenly">
-            <v-btn color="#B3E5FC" variant="elevated" width="165" height="40" min-width="140" size="small"
-              @click="updateEmployeesBenefits" :disabled="!validfx">
-              Update Benefits | {{ payCheck.fotoz }}
+            <v-btn
+              color="#B3E5FC"
+              variant="elevated"
+              width="165"
+              height="40"
+              min-width="140"
+              size="small"
+              @click="updateEmployeesBenefits"
+              :disabled="!validfx"
+            >
+              Update Employees | {{ payCheck.fotoz }}
             </v-btn>
 
-            <v-btn color="#E0F7FA" variant="elevated" width="165" height="40" min-width="140" size="small"
-              :disabled="loading" :to="{ name: 'Employees', query: { tab: tabX } }">
-              Benefits listed
+            <v-btn
+              color="#E0F7FA"
+              variant="elevated"
+              width="165"
+              height="40"
+              min-width="140"
+              size="small"
+              :disabled="loading"
+              :to="{ name: 'Employees', query: { tab: tabX } }"
+              @click="updateChildrenBenefits"
+            >
+              Update Children
             </v-btn>
           </v-card-actions>
         </v-form>
@@ -133,6 +231,9 @@ const marriage = ["Yes", "No"];
 const childrenx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const name1 = ref("NikolasXXX");
 const gender1 = ref("Male");
+
+// 0977470199
+//0977423299
 
 const validateForm = () => {
   const isWorkedNumeric = !isNaN(parseFloat(payCheck.value.periodYear));
@@ -189,7 +290,38 @@ const updateEmployeesBenefits = async () => {
       marriage: payCheck.value.marriage,
       families: payCheck.value.families,
     };
+    console.log(monthlyBenefit);
+    storeAPI.updateOrAddBenefits(monthlyBenefit);
+    goBackPage();
+  }
+};
 
+const updateChildrenBenefits = async () => {
+  if (payCheck.value.namex) {
+    const married = payCheck.value.marriage === "Yes" ? 1 : 0;
+    const numberOfChildren = payCheck.value.families.length;
+    const filteredAANames = payCheck.value.families.filter(
+      (x: { namex: string }) => x.namex.startsWith("A")
+    );
+    const discountedA = (filteredAANames.length * 500 * 10) / 100;
+    const deducts = (1000 + 500 * numberOfChildren + married * 500) / 12;
+    const monthlyBenefit: Salary = {
+      id: customId.value,
+      namex: payCheck.value.namex,
+      numberx: payCheck.value.numberx,
+      periodMonth: payCheck.value.periodMonth,
+      periodYear: payCheck.value.periodYear,
+      created: payCheck.value.created,
+      paycheck: 2000 * 2,
+      updated: new Date(),
+      emailx: payCheck.value.emailx,
+      costBenefits: deducts,
+      children: numberOfChildren,
+      discounted: discountedA,
+      grosspay: 2000 * 2 - deducts,
+      marriage: payCheck.value.marriage,
+      families: payCheck.value.families,
+    };
     console.log(monthlyBenefit);
     const x: Family = {
       id: 1,
